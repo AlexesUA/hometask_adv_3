7019cf4dca8874802894d232a8172409a4500c4f
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-25 20:58:24 +0300

    Finish task

--- src/ua/edu/cbs/lms/hometask_adv_3/task2/Main.java
+++ src/ua/edu/cbs/lms/hometask_adv_3/task2/Main.java
@@ -8,15 +8,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
-        try (FileWriter writer = new FileWriter("test.txt")) {
-            writer.write("Це тестовий рядок даних.");
+        String fileName = System.getProperty("user.dir").replace("\\", "/") +
+                "/src/ua/edu/cbs/lms/hometask_adv_3/task2/sometest.txt";
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write("Some text for write to the file.");
             writer.flush();
         } catch (Exception error) {
             ErrorsHandling.errorsHandling(error);
         }
 
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
